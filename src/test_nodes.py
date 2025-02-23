691890591ac7a7bedb905da1451a4d8a7d318968
ImSandwich21 <daniel-21-silva@hotmail.com>
2025-02-23 21:48:14 +0000

    Add markdown converter

---
+++ src/test_nodes.py
@@ -0,0 +1,14 @@
+import unittest
+from textnode import *
+from nodes import *
+
+class TestNodes(unittest.TestCase):
+    def test_code_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
